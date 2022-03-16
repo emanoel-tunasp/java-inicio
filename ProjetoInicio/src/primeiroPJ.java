9935ed4be7b39475819cfff649fa3b0fa5a568f2
emanoel  tunã 1 <92584275+emanoel-tunasp@users.noreply.github.com>
2022-03-16 16:13:16 -0300

    Meu primeiro projeto java

---
+++ ProjetoInicio/src/primeiroPJ.java
@@ -0,0 +1,16 @@
+
+public class primeiroPJ{
+
+	public static void main(String[] args) {
+		int idade = 22;
+		String nome = "emanoel" ; 
+		 
+		System.out.println("Olá mundo!");
+		System.out.println("Seja bem vindo "+nome);
+		System.out.println("sua idade:"+idade);
+		
+		
+		
+	}
+
+}
