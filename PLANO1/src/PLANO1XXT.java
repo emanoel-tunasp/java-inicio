2ebc5852a4b5c96b6eb134e6861763032fe02e20
emanoel  tunã 1 <92584275+emanoel-tunasp@users.noreply.github.com>
2022-03-25 10:35:32 -0300

    Exercício 01

---
+++ PLANO1/src/PLANO1XXT.java
@@ -0,0 +1,36 @@
+
+
+import java.util.Scanner;
+
+public class PLANO1XXT {
+
+	public static void main(String[] args) {
+
+		Scanner leitor = new Scanner(System.in);
+	int numero;
+System.out.println("POSITIVO OU NEGATIVO");
+numero=leitor.nextInt();
+
+if(numero >= 0 ) {
+	System.out.println(" e POSITIVO");
+}
+
+else {
+	if(numero <0 ) 
+		System.out.println(" e NEGATIVO");
+}
+
+
+
+
+
+
+
+
+
+
+
+		leitor.close();
+	}
+
+}
