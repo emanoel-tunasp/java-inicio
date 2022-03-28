85a785d872021b29f198e43c57f3beb621eceaa8
emanoel  tunã 1 <92584275+emanoel-tunasp@users.noreply.github.com>
2022-03-28 07:39:10 -0300

    operadores de atribuição cumulativa

--- PLANO1/src/PLANO1XXT.java
+++ PLANO1/src/PLANO1XXT.java
@@ -1,20 +1,21 @@
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class PLANO1XXT {
 
 	public static void main(String[] args) {
-
+		Locale.setDefault(Locale.US);
 		Scanner leitor = new Scanner(System.in);
-		System.out.println("=====IMPAR OU PAR=====");
-		System.out.println("Digite um numero");
-		int numero = leitor.nextInt();
 
-		if (numero % 2 == 0) {
-			System.out.println("PAR");
-		} else {
-			System.out.println("IMPAR");
+		int minutos = leitor.nextInt();
+		
+		
+		double conta = 50.00;
+		if (minutos > 100) {
+			conta = conta + (minutos - 100) * 2.0;
 		}
+		System.out.printf("Valor da conta = R$%.2f%n",conta);
 
 		leitor.close();
 	}
