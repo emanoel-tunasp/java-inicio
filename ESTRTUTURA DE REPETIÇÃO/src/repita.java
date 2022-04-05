bf97308dc06bcdd1900867863ef75b744fd213c8
emanoel  tunã 1 <92584275+emanoel-tunasp@users.noreply.github.com>
2022-04-05 07:22:39 -0300

    repetição
    for

--- ESTRTUTURA DE REPETIÇÃO/src/repita.java
+++ ESTRTUTURA DE REPETIÇÃO/src/repita.java
@@ -3,19 +3,10 @@ import java.util.Scanner;
 public class repita {
 
 	public static void main(String[] args) {
-	 
-Scanner sc = new Scanner (System.in);
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Valor de i:  " + i);
+		}
 
-int N = sc.nextInt();
-
-int soma = 0;
-for (int i=0; i<N; i++) {
-	int x = sc.nextInt();
-	soma = soma + x;
-}
-System.out.println(soma);
-
-sc.close();
 	}
 
 }
