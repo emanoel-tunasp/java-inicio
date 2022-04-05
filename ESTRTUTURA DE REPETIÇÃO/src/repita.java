f3f1e7b45b37cde5b78185af9716f627e3cf302a
emanoel  tunã 1 <92584275+emanoel-tunasp@users.noreply.github.com>
2022-04-05 11:17:24 -0300

    maior numero
    if else

--- ESTRTUTURA DE REPETIÇÃO/src/repita.java
+++ ESTRTUTURA DE REPETIÇÃO/src/repita.java
@@ -4,15 +4,19 @@ public class repita {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int mask = 0b00100000;
-		int n = sc.nextInt();
+		System.out.println("Enter three numbers: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
 
-		if ((n & mask) != 0) {
-			System.out.println("6 th bit is true !");
+		if (a > b && a > c) {
+			System.out.println("Higher = " + a);
+		} else if (b > c) {
+			System.out.println("Higher = " + b);
 		} else {
-			System.out.println("6th bit is false!");
-
+			System.out.println("Higher = " + c);
 		}
 
+		sc.close();
 	}
 }
