fdc48f93bb7b44356e129fb087e15c80a5a18040
emanoel  tunã 1 <92584275+emanoel-tunasp@users.noreply.github.com>
2022-04-26 21:36:26 -0300

    Rectangle
     atributos e métodos)

--- Employee/src/application/Program.java
+++ Employee/src/application/Program.java
@@ -1,29 +1,20 @@
 package application;
-
 import java.util.Locale;
 import java.util.Scanner;
-import entities.Employee;
 
+import entities.Employee;
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		Employee emp = new Employee();
-		System.out.print("Name: ");
-		emp.name = sc.nextLine();
-		System.out.print("Gross salary: ");
-		emp.grossSalary = sc.nextDouble();
-		System.out.print("Tax: ");
-		emp.tax = sc.nextDouble();
-		System.out.println();
-		System.out.println("Employee: " + emp);
-		System.out.println();
-		System.out.print("Which percentage to increase salary? ");
-		double percentage = sc.nextDouble();
-		emp.increaseSalary(percentage);
-		System.out.println();
-		System.out.println("Updated data: " + emp);
+		Employee rect = new Employee();
+		System.out.println("Enter rectangle width and height:");
+		rect.width = sc.nextDouble();
+		rect.height = sc.nextDouble();
+		System.out.printf("AREA = %.2f%n", rect.area());
+		System.out.printf("PERIMETER = %.2f%n", rect.perimeter());
+		System.out.printf("DIAGONAL = %.2f%n", rect.diagonal());
 		sc.close();
-	}
-}
+		}
+		}
--- Employee/src/entities/Employee.java
+++ Employee/src/entities/Employee.java
@@ -1,19 +1,16 @@
 package entities;
 
 public class Employee {
-	public String name;
-	public double grossSalary;
-	public double tax;
-
-	public double netSalary() {
-		return grossSalary - tax;
+	
+	public double width;
+	public double height;
+	public double area() {
+	return width * height;
 	}
-
-	public void increaseSalary(double percentage) {
-		grossSalary += grossSalary * percentage / 100.0;
+	public double perimeter() {
+	return 2 * (width + height);
 	}
-
-	public String toString() {
-		return name + ", $ " + String.format("%.2f", netSalary());
+	public double diagonal() {
+	return Math.sqrt(width * width + height * height);
 	}
-}
+	}
