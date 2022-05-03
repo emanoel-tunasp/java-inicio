2dbdd67d29293c590aea2e93cf3ed5b113993d1d
emanoel  tunã 1 <92584275+emanoel-tunasp@users.noreply.github.com>
2022-05-02 22:15:12 -0300

    cachoro
    clases

---
+++ Main/src/dog/cachorro.java
@@ -0,0 +1,13 @@
+package dog;
+
+public class cachorro {
+	int tamanho ;
+	String raca;
+	
+	void latir () {
+		System.out.println("au au!!");
+		
+		
+	}
+
+}
---
+++ Main/src/dog/petchop.java
@@ -0,0 +1,12 @@
+package dog;
+
+public class petchop {
+
+	public static void main(String[] args) {
+		cachorro labrador = new cachorro();
+		labrador.raca = "labrador";
+		labrador.tamanho = 40 ;
+		labrador.latir();
+	}
+
+}
